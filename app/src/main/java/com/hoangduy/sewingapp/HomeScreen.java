2a53ef7f168f8a0d8cc859d674938092629ab398
Hoàng Duy <hoangduy@Duys-MacBook-Pro.local>
2018-02-12 21:37:33 +0700

    Added NewCustomerScreen UI

--- app/src/main/java/com/hoangduy/sewingapp/HomeScreen.java
+++ app/src/main/java/com/hoangduy/sewingapp/HomeScreen.java
@@ -1,13 +1,32 @@
 package com.hoangduy.sewingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
+
+    private FloatingActionButton fabAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        //bind view
+        fabAdd = findViewById(R.id.fabAdd);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToDetail();
+            }
+        });
+    }
+
+    private void navigateToDetail() {
+        Intent intent = new Intent(HomeScreen.this, CustomerAddScreen.class);
+        startActivity(intent);
     }
 }
